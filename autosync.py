6f54c70a7603d3ad95e6f3aeaa83681541c61a9b
delta1512 <markbel555@gmail.com>
2017-04-11 21:52:11 +1000

    Fixed difference detection

--- autosync.py
+++ autosync.py
@@ -3,53 +3,71 @@ import os
 import glob
 import datetime
 
-def useless(function):
-    pass
+global months
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-root = _folder to sync
+def time_check(cli, ser):
+    if months.index(cli[1]) == months.index(ser[1]):
+        if cli[2] == ser[2]:
+            if cli[3][:-3] == ser[3][:-3]:
+                return (int(cli[3][3:]) > int(ser[3][3:])-1 and
+                        int(cli[3][3:]) > int(ser[3][3:])+1)
+            else:
+                return (int(cli[3][:-3]) > int(ser[3][:-3]))
+        else:
+            return (int(cli[2]) > int(ser[2]))
+    else:
+        return (months.index(cli[1]) > months.index(ser[1]))
+
+root = '''root of sync directory'''
 os.chdir(root)
-file_list = glob.glob('*')
-for i, f in enumerate(file_list):
-    file_list[i] = [f]
-    file_list[i].append(datetime.datetime.fromtimestamp(int(os.path.getmtime(f))).strftime('%b %d %H:%M').split())
-    for x in file_list[i][1]:
-        file_list[i].append(x)
-    file_list[i].pop(1)
+clnt_files = glob.glob('*')
+for i, f in enumerate(clnt_files):
+    clnt_files[i] = [f]
+    clnt_files[i].append(datetime.datetime.fromtimestamp(int(os.path.getmtime(f))).strftime('%b %d %H:%M').split())
+    for x in clnt_files[i][1]:
+        clnt_files[i].append(x)
+    clnt_files[i].pop(1)
 
-with FTP(_server) as ftp:
-    ftp.login(user=_username, passwd=_password)
-    flist = []
-    ftp.retrlines('LIST', flist.append)
-    for i, x in enumerate(flist):
-        flist[i] = x.split()
+with FTP('''hostname of ftp server''') as ftp:
+    ftp.login(user='''account for ftp server''', passwd='''passwd for account''')
+    serv_files = []
+    ftp.retrlines('LIST', serv_files.append)
+    for i, x in enumerate(serv_files):
+        serv_files[i] = x.split()
     tmp = []
-    for i, x in enumerate(flist):
+    for i, x in enumerate(serv_files):
         tmp.append([])
         tmp[i].append(x[-1])
         tmp[i].append(x[5])
         tmp[i].append(x[6])
         tmp[i].append(x[7])
-    flist = tmp
+    serv_files = tmp
     del tmp
     put = []
-    for clientf in file_list:
-        diff = True
-        for servf in flist:
-            if clientf == servf:
-                diff = False
-        if diff:
+    for clientf in clnt_files:
+        found = False
+        for servf in serv_files:
+            if clientf[0] == servf[0]:
+                found = True
+                if clientf[0] == servf[0] and time_check(clientf, servf):
+                    put.append(clientf[0])
+        if not found:
             put.append(clientf[0])
     get = []
-    for servf in flist:
-        diff = True
-        for clientf in file_list:
-            if servf == clientf:
-                diff = False
-        if diff:
+    for servf in serv_files:
+        found = False
+        for clientf in clnt_files:
+            if servf[0] == clientf[0]:
+                found = True
+                if time_check(servf, clientf):
+                    get.append(servf[0])
+        if not found:
             get.append(servf[0])
     for f in put:
         ftp.storbinary('STOR ' + f, open(f, 'rb'))
         t = int(os.popen('date +%s').read())
         os.utime(f,(t, t))
     for f in get:
-        ftp.retrbinary('RETR ' + f, useless)
+        ftp.retrbinary('RETR ' + f, open(f, 'wb').write)
